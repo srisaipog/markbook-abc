cfad894eb23fd28b4006723f6d9c364cffdb1686
srisaipog <sridhar.sairam21@ycdsbk12.ca>
2019-09-12 10:59:31 -0400

    Changed files

--- markbook.py
+++ markbook.py
@@ -2,7 +2,27 @@
 Markbook Application
 Group members: 
 """
+from typing import Dict
 
 
-def some_func():
-    return True
+def create_assignment(name: str, due: str, points: int) -> Dict:
+    """Creates an assignment represented as a dictionary
+    
+    Args:
+        name: the name of the assignment.
+        due: the due date for the assignment.
+        points: what the assignment is out of (denominator).
+    Returns:
+        Assignment as a dictionary.
+    """
+    return {}
+
+
+def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
+    """Creates a classroom dictionary"""
+    return {}
+
+
+def calculate_average_mark(student: Dict) -> float:
+    """Calculates the average mark of a student"""
+    return 0
--- test_markbook.py
+++ test_markbook.py
@@ -1,9 +1,53 @@
-    import markbook
+import pytest
+
+import markbook
 
 
-def test_can_run_tests():
-    assert True is True
+@pytest.mark.skip
+def test_create_assigment():
+    assignment1 = markbook.create_assignment(name="Assignment One",
+                                            due="2019-09-21",
+                                            points=100)
+    expected = {
+        "name": "Assignment One",
+        "due": "2019-09-21",
+        "points": 100
+    }
+    assert assignment1 == expected
+
+    assignment2 = markbook.create_assignment(name="Assignment Two",
+                                        due=None,
+                                        points=1)
+    assert assignment2["name"] == "Assignment Two"
+    assert assignment2["due"] is None
+    assert assignment2["points"] == 1
 
 
-def test_can_test_markbook():
-    assert markbook.some_func() is True
+@pytest.mark.skip()
+def test_create_classroom():
+    classroom = markbook.create_classroom(course_code="ICS4U",
+                                          course_name="Computer Science",
+                                          period=2,
+                                          teacher="Mr. Gallo")
+    expected = {
+        "course_code": "ICS4U",
+        "course_name": "Computer Science",
+        "period": 2,
+        "teacher": "Mr. Gallo"
+    }
+
+    # The classroom needs to be a dictionary identical to the expected
+    assert classroom == expected
+
+    # The classroom needs to be created with
+    # empty lists for students and assignments
+    assert classroom["student_list"] == []
+    assert classroom["assignment_list"] == []
+
+
+@pytest.mark.skip
+def test_calculate_average_mark():
+    student = {
+        "marks": [50, 100]
+    }
+    assert markbook.calculate_average_mark(student) == 75.0
