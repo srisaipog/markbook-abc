e473bcd9254b63d3e78b4e76a2b9fc30a64b5744
sarah888 <43208342+sarah888@users.noreply.github.com>
2019-09-24 11:01:17 -0400

    Update assignment-functions.py
    Updated new_assignment()

--- functions/assignment-functions.py
+++ functions/assignment-functions.py
@@ -1,5 +1,5 @@
 def new_assignment():
-    add_assignment = dict()
+    all_assignments = dict()
     
     key_1 = 'assignment_title'
     value_1 = input("Enter the assignment title: ")
@@ -8,8 +8,9 @@ def new_assignment():
     while True:
         try:
             value_2 = int(input("Enter the month of due date (MM): "))
-            if value_2 <= 12:
+            if value_2 <= 12 and value_2 >= 1:
                 break
+            
         except ValueError:
             print("That was not a valid number. Try again.")
 
@@ -17,30 +18,31 @@ def new_assignment():
     while True:
         try:
             value_3 = int(input("Enter the day of due date (DD): "))
-            if value_2 == 1 and value_3 <= 31:
+            if value_2 == 1 and value_3 <= 31 and value_3 >= 1 :
                 break
-            elif value_2 == 2 and value_3 <= 28:
+            elif value_2 == 2 and value_3 <= 28 and value_3 >= 1:
                 break
-            elif value_2 == 3 and value_3 <= 31:
+            elif value_2 == 3 and value_3 <= 31 and value_3 >= 1:
                 break
-            elif value_2 == 4 and value_3 <= 30:
+            elif value_2 == 4 and value_3 <= 30 and value_3 >= 1:
                 break
-            elif value_2 == 5 and value_3 <= 31:
+            elif value_2 == 5 and value_3 <= 31 and value_3 >= 1:
                 break
-            elif value_2 == 6 and value_3 <= 30:
+            elif value_2 == 6 and value_3 <= 30 and value_3 >= 1:
                 break
-            elif value_2 == 7 and value_3 <= 31:
+            elif value_2 == 7 and value_3 <= 31 and value_3 >= 1:
                 break
-            elif value_2 == 8 and value_3 <= 31:
+            elif value_2 == 8 and value_3 <= 31 and value_3 >= 1:
                 break
-            elif value_2 == 9 and value_3 <= 30:
+            elif value_2 == 9 and value_3 <= 30 and value_3 >= 1:
                 break
-            elif value_2 == 10 and value_3 <= 31:
+            elif value_2 == 10 and value_3 <= 31 and value_3 >= 1:
                 break
-            elif value_2 == 11 and value_3 <= 30:
+            elif value_2 == 11 and value_3 <= 30 and value_3 >= 1:
                 break
-            elif value_2 == 12 and value_3 <= 31:
+            elif value_2 == 12 and value_3 <= 31 and value_3 >= 1:
                 break
+            
         except ValueError:
             print("That was not a valid date. Try again.")
 
@@ -49,7 +51,7 @@ def new_assignment():
     while True:
         try:
             value_4 = int(input("Enter the year of due date (YYYY): "))
-            if value_4 <= 2100:
+            if value_4 >= 2018:
                 break
         except ValueError:
             print("That was not a valid number. Try again.")
@@ -62,11 +64,12 @@ def new_assignment():
         except ValueError:
             print("That was not a valid number. Try again.")
 
-    add_assignment[key_1] = value_1
-    add_assignment[key_2] = value_2
-    add_assignment[key_3] = value_3
-    add_assignment[key_4] = value_4
-    add_assignment[key_5] = value_5
+    all_assignments[key_1] = value_1
+    all_assignments[key_2] = value_2
+    all_assignments[key_3] = value_3
+    all_assignments[key_4] = value_4
+    all_assignments[key_5] = value_5
 
-    return add_assignment
+    return all_assignments
     
+new_assignment()
