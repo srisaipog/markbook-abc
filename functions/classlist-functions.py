94b6c5589ea7d49c408b6d19105428c8dabbf6cb
--globalmathieultc <--globalMathieu.litimcheong20@ycdsbk12.ca>
2019-09-25 23:53:42 -0400

    .

--- functions/classlist-functions.py
+++ functions/classlist-functions.py
@@ -1,5 +1,47 @@
 from typing import Dict
 
+def new_classroom():
+    classroom = dict()
+
+    key_1 = "coursecode"
+    value_1 = input("Enter the course code: ")
+
+    key_2 = "coursename"
+    value_2 = input("Enter the course name here: ")
+
+    key_3 = "period"
+    while True:
+        try:
+            value_3 = int(input("Enter class period(1,2,3,4): "))
+            if value_3 <= 4 and value_3 >= 1:
+                break
+            else:
+                print("That was not a valid answer. Try again")
+        
+        except ValueError:
+            print("That was not a valid answer. Try again")
+
+    key_4 = "teacher"
+    value_4 = input("Enter teacher's name here: ")
+
+    key_5 = "student_list"
+    value_5 = []
+
+    key_6 = "Assignment_list"
+    value_6 = []
+
+    classroom[key_1] = value_1
+    classroom[key_2] = value_2
+    classroom[key_3] = value_3
+    classroom[key_4] = value_4
+    classroom[key_5] = value_5
+    classroom[key_6] = value_6
+
+    return classroom
+    
+new_classroom()
+
+
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
     classroom = {'Coursecode': course_code,
     'Course name': course_name,
@@ -37,3 +79,4 @@ def edit_student(student: Dict, **kwargs: Dict):
             of a dictionary.
     """
     pass
+
