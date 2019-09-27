e9df25b994afcd11e2ac4cee5c3ca43d364f999e
sarah888 <43208342+sarah888@users.noreply.github.com>
2019-09-27 10:27:22 -0400

    can now have no due date

--- functions/assignment-functions.py
+++ functions/assignment-functions.py
@@ -8,8 +8,8 @@ def create_assignment():
 # input month
     while True:
         try:
-            value_month = int(input("Enter the month of due date (MM): "))
-            if value_month <= 12 and value_month >= 1:
+            value_month = int(input("Enter the month of due date (MM) (enter 0 for no due date): "))
+            if value_month <= 12 and value_month >= 0:
                 break
             else:
                 print("That was not a valid month. Try again.")
@@ -17,51 +17,57 @@ def create_assignment():
         except ValueError:
             print("That was not a valid number. Try again.")
 
-# input date
-    while True:
-        try:
-            value_day = int(input("Enter the day of due date (DD): "))
-            if value_month == 1 and value_day <= 31 and value_day >= 1 :
-                break
-            elif value_month == 2 and value_day <= 28 and value_day >= 1:
-                break
-            elif value_month == 3 and value_day <= 31 and value_day >= 1:
-                break
-            elif value_month == 4 and value_day <= 30 and value_day >= 1:
-                break
-            elif value_month == 5 and value_day <= 31 and value_day >= 1:
-                break
-            elif value_month == 6 and value_day <= 30 and value_day >= 1:
-                break
-            elif value_month == 7 and value_day <= 31 and value_day >= 1:
-                break
-            elif value_month == 8 and value_day <= 31 and value_day >= 1:
-                break
-            elif value_month == 9 and value_day <= 30 and value_day >= 1:
-                break
-            elif value_month == 10 and value_day <= 31 and value_day >= 1:
-                break
-            elif value_month == 11 and value_day <= 30 and value_day >= 1:
-                break
-            elif value_month == 12 and value_day <= 31 and value_day >= 1:
-                break
-            else:
-                print("That was not a valid date. Try again.")
-    
-        except ValueError:
-            print("That was not a valid date. Try again.")
 
-# input year
-    while True:
-        try:
-            value_year = int(input("Enter the year of due date (YYYY): "))
-            if value_year >= 2019:
-                break
-            else:
-                print("That was not a valid year. Try again.")
-    
-        except ValueError:
-            print("That was not a valid number. Try again.")
+    if value_month == 0:
+        value_month = None
+        value_day = None
+        value_year = None
+    else:
+# input date
+        while True:
+            try:
+                value_day = int(input("Enter the day of due date (DD): "))
+                if value_month == 1 and value_day <= 31 and value_day >= 1 :
+                    break
+                elif value_month == 2 and value_day <= 28 and value_day >= 1:
+                    break
+                elif value_month == 3 and value_day <= 31 and value_day >= 1:
+                    break
+                elif value_month == 4 and value_day <= 30 and value_day >= 1:
+                    break
+                elif value_month == 5 and value_day <= 31 and value_day >= 1:
+                    break
+                elif value_month == 6 and value_day <= 30 and value_day >= 1:
+                    break
+                elif value_month == 7 and value_day <= 31 and value_day >= 1:
+                    break
+                elif value_month == 8 and value_day <= 31 and value_day >= 1:
+                    break
+                elif value_month == 9 and value_day <= 30 and value_day >= 1:
+                    break
+                elif value_month == 10 and value_day <= 31 and value_day >= 1:
+                    break
+                elif value_month == 11 and value_day <= 30 and value_day >= 1:
+                    break
+                elif value_month == 12 and value_day <= 31 and value_day >= 1:
+                    break
+                else:
+                    print("That was not a valid date. Try again.")
+        
+            except ValueError:
+                print("That was not a valid date. Try again.")
+
+    # input year
+        while True:
+            try:
+                value_year = int(input("Enter the year of due date (YYYY): "))
+                if value_year >= 2019:
+                    break
+                else:
+                    print("That was not a valid year. Try again.")
+        
+            except ValueError:
+                print("That was not a valid number. Try again.")
 
     key_points = 'points'
     while True:
@@ -72,7 +78,10 @@ def create_assignment():
             print("That was not a valid number. Try again.")
 
     all_assignments[key_title] = value_title
-    all_assignments[key_date] = f"{value_year} - {value_month} - {value_day}"
+    if value_month == None:
+        all_assignments[key_date] = None
+    else:
+        all_assignments[key_date] = f"{value_year} - {value_month} - {value_day}"
     all_assignments[key_points] = value_points
 
     return all_assignments
