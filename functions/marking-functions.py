103982fd30d1df957cc571eaf64c381245a02bf2
sarah888 <43208342+sarah888@users.noreply.github.com>
2019-09-29 18:07:40 -0400

    new autodrop lowest mark function

---
+++ functions/marking-functions.py
@@ -0,0 +1,7 @@
+def drop_lowest_mark(student_marks_list):
+    lowest_mark = student_marks_list[0]
+    for mark in student_marks_list:
+        if mark < lowest_mark:
+            lowest_mark = mark
+    student_marks_list.remove(lowest_mark)
+    print(student_marks_list)
