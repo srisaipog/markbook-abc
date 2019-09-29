dcb2a0b982932831d94e1d25bed996323cdd9e8c
sarah888 <43208342+sarah888@users.noreply.github.com>
2019-09-29 18:26:46 -0400

    new mark averaging function

--- functions/marking-functions.py
+++ functions/marking-functions.py
@@ -5,3 +5,12 @@ def drop_lowest_mark(student_marks_list):
             lowest_mark = mark
     student_marks_list.remove(lowest_mark)
     return student_marks_list
+
+def average_of_marks(marks_list):
+    mark_count = 0
+    for mark in marks_list:
+        global mark_total
+        mark_count += 1
+        mark_total = sum(marks_list)
+    mark_average = round(mark_total/mark_count, 1)
+    return mark_average
