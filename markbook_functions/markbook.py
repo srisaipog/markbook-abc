76e2b45d01639fce8adb100a83c04660b352c602
srisaipog <43004256+srisaipog@users.noreply.github.com>
2019-09-30 10:47:14 -0400

    Created options

--- markbook_functions/markbook.py
+++ markbook_functions/markbook.py
@@ -1,10 +1,24 @@
-
-
-
 """
 Markbook Application
-Group members: 
+Group members: Matthew, Max, Sarah, Sridhar
 """
-from typing import Dict
 
+import json
+import functions
+
+option = ""
+
+option_letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+option_functions = [new_classroom(), create_classroom(), add_student_to_classroom(), 
+                     remove_student_from_classroom(), edit_student(), create_assignment(),
+                     drop_lowest_mark(), average_of_marks(), create_student(),
+                     calculate_average_mark(), edit_student()]
+
+options = {}
+
+for i in range(len(option_functions):
+  options[option_letters[i]] = option_functions[i]
+
+while True:
+  option = input("What option would you like to choose?")
 
